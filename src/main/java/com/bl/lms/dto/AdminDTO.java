1bac664a90b773205fbfe7a0aa0322dcb8dbc89b
julinbestina <julin.bestina@yahoo.com>
2022-10-11 13:40:47 +0530

    added: initial files for Admin

--- src/main/java/com/bl/lms/dto/AdminDTO.java
+++ src/main/java/com/bl/lms/dto/AdminDTO.java
@@ -2,4 +2,10 @@ package com.bl.lms.dto;
 
 public class AdminDTO {
 
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String mobile;
+	private String profilePath;
+	private String password;
 }
