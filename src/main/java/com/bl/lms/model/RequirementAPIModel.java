8db4b28df0f08c22960d1f12206329015ad300bd
NeetaMourya-BL <neeta.mourya@bridgelabz.com>
2022-10-12 18:42:39 +0530

    Added: Requirement module model class addes

--- src/main/java/com/bl/lms/model/RequirementAPIModel.java
+++ src/main/java/com/bl/lms/model/RequirementAPIModel.java
@@ -1,5 +1,26 @@
 package com.bl.lms.model;
 
-public class RequirementAPIModel {
+import lombok.Data;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Company_Requirement")
+public @Data class RequirementAPIModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Id", nullable = false)
+	private int id;
+	private String companyName;
+	private String requestMonth;
+	private String city;
+	private String requirementPath;
+	private String documentVerification;
+	private String techRequirements;
+	@ElementCollection
+	@CollectionTable(name = "TechRequirements", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "totalEngineers")
+	private List<String> TechRequirements;
 
 }
