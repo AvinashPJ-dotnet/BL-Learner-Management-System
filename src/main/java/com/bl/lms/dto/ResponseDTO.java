9b9b444f5ca08615ab5637eee9ea45db5cc39ed8
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-10-14 15:50:19 +0530

    Modified: ResponseDTO conflict

--- src/main/java/com/bl/lms/dto/ResponseDTO.java
+++ src/main/java/com/bl/lms/dto/ResponseDTO.java
@@ -2,9 +2,9 @@ package com.bl.lms.dto;
 
 import lombok.Data;
 
-public @Data class ResponseDTO {
+ @Data 
+ public class ResponseDTO {
 
-<<<<<<< HEAD
 	private String message;
 	private Object data;
 
@@ -12,13 +12,4 @@ public @Data class ResponseDTO {
 		this.message = string;
 		this.data = payrollData;
 	}
-=======
-    private String message;
-    private Object data;
-
-    public ResponseDTO(String string, Object payrollData) {
-        this.message = string;
-        this.data = payrollData;
-    }
->>>>>>> Hiring_Module_Added_Model_001
 }
